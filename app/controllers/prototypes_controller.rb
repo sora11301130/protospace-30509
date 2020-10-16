9cc025d08ad26b375a889ca2e09b655149ca1d0c
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-16 17:05:46 +0900

    Issue1回目

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,5 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except:[:index, :show]
-  before_action :move_to_index, only:[:new, :create, :edit, :update, :destroy]
   
 
 
@@ -48,15 +47,11 @@ class PrototypesController < ApplicationController
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
+    redirect_to root_path
   end
    
   private
-  def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless user_signed_in? && current_user.id == @prototype.user.id
-      redirect_to action: :index
-    end
-  end
+
 
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
