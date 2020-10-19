bc051d8ee42655201f683fe371c0d655b1d46b57
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-19 14:55:13 +0900

    Issue5

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -35,14 +35,12 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
       redirect_to prototype_path(@prototype)
     else
       render :edit
     end
-
-
   end
 
   def destroy
