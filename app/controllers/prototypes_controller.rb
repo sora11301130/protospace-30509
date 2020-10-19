3c2aacddef153c7f34b53f4cc410836eb3a1ee88
sora11301130 <71582397+sora11301130@users.noreply.github.com>
2020-10-19 12:09:11 +0900

    Issue4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -24,7 +24,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments.includes(:user)
   end
   
   def edit
